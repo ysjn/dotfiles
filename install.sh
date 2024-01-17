c07964ab4cab32ac084676bda797ce9ea8b7e10b
jyoshida <jyoshida@lycorp.co.jp>
2024-01-17 14:24:49 +0900

    update install script

--- install.sh
+++ install.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# check if homebrew is installed
+which -s brew
+if [[ $? != 0 ]]; then
+	# Install Homebrew
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+	# update PATH
+	echo 'eval $(/opt/homebrew/bin/brew shellenv)' >>/Users/jyoshida/.zprofile
+	eval $(/opt/homebrew/bin/brew shellenv)
+else
+	brew update
+fi
+
+# install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
 # install neovim
 brew install neovim ripgrep fd lazygit
 
@@ -14,13 +30,16 @@ rm -rf ~/.config/nvim/.git
 
 # install fonts
 brew tap homebrew/cask-fonts
-brew install --cask font-jetbrains-mono
+brew install --cask font-jetbrains-mono-nerd-font
 
 # install github completion
 mkdir -p ~/.zsh
 curl -o ~/.zsh/.git-completion.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 curl -o ~/.zsh/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+source ~/.zsh/.git-completion.sh
+source ~/.zsh/.git-prompt.sh
 
 # create symbolic link
 ln -snfv ~/dev/dotfiles/.zshrc ~
+rm -rf ~/.config/nvim
 ln -snfv ~/dev/dotfiles/nvim ~/.config
