f71a015f3c5dd30e5423ead6b35c7632168d6124
jyoshida <jyoshida@lycorp.co.jp>
2024-02-13 18:18:39 +0900

    better config

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -54,38 +54,44 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-telescope/telescope-live-grep-args.nvim" },
-    config = function()
-      require("telescope").setup({
+    opts = function()
+      local actions = require("telescope.actions")
+      require("telescope").load_extension("live_grep_args")
+      return {
         defaults = {
           file_ignore_patterns = { "/node_modules/", "package-lock.json", "yarn.lock" },
           mappings = {
             n = {
               ["q"] = "close",
               ["<C-a>"] = function(p_bufnr)
-                require("telescope.actions").send_selected_to_qflist(p_bufnr)
+                actions.send_selected_to_qflist(p_bufnr)
                 vim.cmd.cfdo("edit")
               end,
               ["<C-r>"] = function(p_bufnr)
                 -- send results to quick fix list
-                require("telescope.actions").send_to_qflist(p_bufnr)
+                actions.send_to_qflist(p_bufnr)
+
                 local qflist = vim.fn.getqflist()
                 local paths = {}
                 local hash = {}
                 for k in pairs(qflist) do
-                  local path = vim.fn.bufname(qflist[k]["bufnr"]) -- extract path from quickfix list
+                  local path = vim.fn.bufname(qflist[k]["bufnr"]) -- extract path from quick fix list
                   if not hash[path] then -- add to paths table, if not already appeared
                     paths[#paths + 1] = path
                     hash[path] = true -- remember existing paths
                   end
                 end
+
+                -- show search scope with message
+                vim.notify("find in ...\n  " .. table.concat(paths, "\n  "))
+
                 -- execute live_grep_args with search scope
                 require("telescope").extensions.live_grep_args.live_grep_args({ search_dirs = paths })
               end,
             },
           },
         },
-      })
-      require("telescope").load_extension("live_grep_args")
+      }
     end,
   },
 
