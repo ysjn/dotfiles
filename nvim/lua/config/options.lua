c712cc4b7219d28db84e2ec7803cdad5b67711ea
jyoshida <jyoshida@lycorp.co.jp>
2024-04-25 18:08:36 +0900

    more scrolloff

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,7 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.breakindent = true
-vim.opt.scrolloff = 15
+vim.opt.scrolloff = 30
 vim.opt.foldmethod = "indent"
 
 vim.diagnostic.config({
