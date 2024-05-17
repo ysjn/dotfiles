3c1f8e3eb7120525f5afc33ea8c9889eda639c39
jyoshida <jyoshida@lycorp.co.jp>
2024-05-17 17:14:12 +0900

    add autocommand for highlighting area on yank

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -34,3 +34,11 @@ vim.api.nvim_create_autocmd("CmdwinLeave", {
     vim.keymap.del("n", "<esc>")
   end,
 })
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
