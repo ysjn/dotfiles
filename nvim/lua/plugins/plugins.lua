53e0c43ebe92547bddf6a522a144293a78ad024e
jyoshida <jyoshida@lycorp.co.jp>
2024-08-18 13:31:19 +0900

    show hidden comments with find files on Telescope

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -243,6 +243,9 @@ return {
             },
           },
         },
+        pickers = {
+          find_files = { hidden = true }, -- include hidden files and directories
+        },
       }
     end,
   },
