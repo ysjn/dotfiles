a56a8ef66af596904b78b29995fe4a0b95e71a31
jyoshida <jyoshida@lycorp.co.jp>
2025-02-15 13:16:33 +0900

    add smear-cursor extras

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -29,6 +29,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.ai.copilot-chat" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.ui.mini-indentscope" },
+    { import = "lazyvim.plugins.extras.ui.smear-cursor" },
 
     -- import/override with your plugins
     { import = "plugins" },
