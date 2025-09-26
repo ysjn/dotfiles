e85a79ede9d406c4c0c2424b9ab70613f842c746
jyoshida <jyoshida@lycorp.co.jp>
2025-09-26 16:58:13 +0900

    add google-japanese-ime

--- install_optional.sh
+++ install_optional.sh
@@ -3,14 +3,14 @@
 # check if homebrew is installed
 which -s brew
 if [[ $? != 0 ]]; then
-	# Install Homebrew
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  # Install Homebrew
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-	# update PATH
-	echo 'eval $(/opt/homebrew/bin/brew shellenv)' >>$HOME/.zprofile
-	eval $(/opt/homebrew/bin/brew shellenv)
+  # update PATH
+  echo 'eval $(/opt/homebrew/bin/brew shellenv)' >>$HOME/.zprofile
+  eval $(/opt/homebrew/bin/brew shellenv)
 else
-	brew update
+  brew update
 fi
 
 # raycast
@@ -19,3 +19,6 @@ brew install --cask raycast
 # pearcleaner
 brew tap alienator88/homebrew-cask
 brew install --cask pearcleaner
+
+# Google Japanese Input Method Editor
+brew install --cask google-japanese-ime
