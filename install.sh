b56e26c711b0a2eb86fa0a0fa22275280943fe02
jyoshida <jyoshida@lycorp.co.jp>
2025-10-04 17:57:47 +0900

    update LazyVim

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 brew install --cask alacritty
 
 # install neovim
-brew install neovim ripgrep fd lazygit
+brew install neovim ripgrep fd lazygit tree-sitter-cli
 
 # install LazyVim
 mv $HOME/.config/nvim{,.bak}
