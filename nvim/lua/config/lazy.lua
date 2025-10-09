cc47672eaeefbbb4d8562eb7800fe44da27fa13f
jyoshida <jyoshida@lycorp.co.jp>
2025-10-09 10:53:30 +0900

    fix: remove claudecode.nvim, add sidekick.nvim

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -27,6 +27,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.ai.copilot" },
+    { import = "lazyvim.plugins.extras.ai.sidekick" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.ui.mini-indentscope" },
     { import = "lazyvim.plugins.extras.ui.smear-cursor" },
--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -307,32 +307,42 @@ return {
   },
 
   {
-    "coder/claudecode.nvim",
-    dependencies = {
-      {
-        "folke/snacks.nvim",
-        -- workaround for pane switch not working in coder/claudecode.nvim
-        -- https://github.com/coder/claudecode.nvim/issues/53#issuecomment-3146728231
-        keys = {
-          { "<A-h>", "<cmd>TmuxNavigateLeft<cr>", mode = "t" },
-          { "<A-l>", "<cmd>TmuxNavigateRight<cr>", mode = "t" },
+    "folke/sidekick.nvim",
+    opts = {
+      cli = {
+        win = {
+          keys = {
+            navigate_left = {
+              "<A-h>",
+              function()
+                vim.cmd("TmuxNavigateLeft")
+              end,
+            },
+            navigate_right = {
+              "<A-l>",
+              function()
+                vim.cmd("TmuxNavigateRight")
+              end,
+            },
+          },
         },
       },
     },
-    opts = {
-      focus_after_send = true,
-    },
     keys = {
-      { "<leader>a", nil, desc = "AI/Claude Code" },
-      { "<leader>aa", "<cmd>ClaudeCode<cr>", desc = "Toggle Claude" },
-      { "<leader>af", "<cmd>ClaudeCodeFocus<cr>", desc = "Focus Claude" },
-      { "<leader>ar", "<cmd>ClaudeCode --resume<cr>", desc = "Resume Claude" },
-      { "<leader>aC", "<cmd>ClaudeCode --continue<cr>", desc = "Continue Claude" },
-      { "<leader>ab", "<cmd>ClaudeCodeAdd %<cr>", desc = "Add current buffer" },
-      { "<leader>ae", "<cmd>ClaudeCodeSend<cr>", mode = "v", desc = "Send to Claude" },
-      { "<leader>ae", "<cmd>ClaudeCodeTreeAdd<cr>", desc = "Add file", ft = { "oil" } },
-      { "<leader>ay", "<cmd>ClaudeCodeDiffAccept<cr>", desc = "Accept diff" },
-      { "<leader>an", "<cmd>ClaudeCodeDiffDeny<cr>", desc = "Deny diff" },
+      {
+        "<leader>aa",
+        function()
+          require("sidekick.cli").toggle({ filter = { installed = true } })
+        end,
+        desc = "Sidekick Toggle",
+      },
+      {
+        "<leader>as",
+        function()
+          require("sidekick.cli").select({ filter = { installed = true } })
+        end,
+        desc = "Sidekick Select CLI",
+      },
     },
   },
 }
