3b136bcbdacd7b797d2f6f4eb30550fa1fa32dc2
jyoshida <jyoshida@lycorp.co.jp>
2025-11-10 17:52:06 +0900

    fix: mv extras to lazyvim.json

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -17,21 +17,6 @@ require("lazy").setup({
       },
     },
 
-    -- import any extras modules here
-    { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.linting.eslint" },
-    { import = "lazyvim.plugins.extras.formatting.prettier" },
-    { import = "lazyvim.plugins.extras.formatting.biome" },
-    { import = "lazyvim.plugins.extras.editor.leap" },
-    { import = "lazyvim.plugins.extras.lsp.none-ls" },
-    { import = "lazyvim.plugins.extras.coding.mini-surround" },
-    { import = "lazyvim.plugins.extras.ai.copilot" },
-    { import = "lazyvim.plugins.extras.ai.sidekick" },
-    { import = "lazyvim.plugins.extras.ui.mini-animate" },
-    { import = "lazyvim.plugins.extras.ui.mini-indentscope" },
-    { import = "lazyvim.plugins.extras.ui.smear-cursor" },
-
     -- import/override with your plugins
     { import = "plugins" },
   },
